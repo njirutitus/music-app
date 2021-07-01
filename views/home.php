381e3347106eb74bf475820660a45db51d701d4f
titus njiru <njirutitus@gmail.com>
2021-07-01 22:57:49 +0300

    adding home view

---
+++ views/home.php
@@ -0,0 +1,8 @@
+<section class="container" style="min-height: 100%">
+    <div class="hero">
+        <div class="hero-text">
+            <h3>Welcome to Viberr</h3>
+            <p>Viberr is an application that let's you upload, store, and play all of your music from the cloud. You
+                can now manage and listen to your music from any device, anywhere in the world.</p>
+        </div>
+</section>
