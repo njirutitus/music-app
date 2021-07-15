fc8433dcbf252d7802c4b27e4e6b7e25448ee90d
titus njiru <njirutitus@gmail.com>
2021-07-15 18:30:16 +0300

    adding css file

---
+++ handlecontact.php
@@ -0,0 +1,6 @@
+<?php
+
+echo 'handling contact';
+header('location: index.php');
+
+?>
---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/main.css">
+    <title>Home</title>
+</head>
+
+<body>
+    <div class="container">
+        <!-- Navigation bar -->
+        <nav>
+            <a>Account</a>
+            <a>Help</a>
+            <a>Logout</a>
+        </nav>
+
+        <!-- Main page area-->
+        <main>
+            <aside>
+                <ul class="nav">
+                    <li>
+                        <a href="">Home</a>
+                    </li>
+                    <li>
+                        <a href="#products">Products</a>
+
+                    </li>
+                    <li>
+                        <a href="#orders">Orders</a>
+                    </li>
+                    <li>
+                        <a href="#users">Users</a>
+                    </li>
+                </ul>
+            </aside>
+            <article>
+                <div id="products">
+                    <h1>Products</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id iusto provident ipsam dolorem iste,
+                        aut asperiores quasi quisquam. Exercitationem ullam architecto deleniti nam, inventore
+                        blanditiis mollitia officia voluptatem sint quia quod dignissimos harum dolore reprehenderit id
+                        ea nulla temporibus voluptate soluta. Voluptatem quas corporis eveniet unde repudiandae nam
+                        similique magnam facilis pariatur inventore odio consequuntur ex quos tempora, natus iusto
+                        recusandae laboriosam nihil obcaecati ad molestiae soluta! Distinctio dolor, atque perferendis
+                        obcaecati dignissimos ex architecto molestias ipsum dolorum. Voluptatibus animi adipisci dolores
+                        nihil in placeat velit nisi eum quos optio saepe ducimus facilis deleniti, aliquid ipsam ab
+                        provident iusto fugiat fuga quis esse explicabo ex. Quod veritatis minus sit nostrum animi porro
+                        maxime quia sunt nobis. Dolorum assumenda optio repudiandae tempora expedita illum placeat
+                        perspiciatis magni omnis aliquid quaerat itaque veritatis voluptatem temporibus vel iusto
+                        ratione, dolorem excepturi nesciunt! Impedit vitae, molestias quo, tenetur temporibus facilis
+                        rerum veritatis accusamus quasi sapiente dolorem laborum aliquid voluptatem repellat quis
+                        debitis numquam qui commodi nisi, maiores nesciunt quos quas? Autem nisi totam et id error
+                        deserunt eos, amet, consectetur cum delectus natus. Aliquam eligendi cupiditate eaque non. Rerum
+                        voluptatibus excepturi facere commodi possimus sed, fugit dignissimos minus quas? Aliquid ad
+                        officia quo maxime.</p>
+                </div>
+                <div id="orders">
+                    <h1>Orders</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum omnis ratione totam eveniet, in
+                        deleniti quibusdam impedit amet? Culpa ex dolorum perferendis earum sit, non hic facere fuga nam
+                        ad? Quae aliquam corrupti eius aspernatur ex? A tempora corrupti doloremque placeat animi,
+                        perferendis iure nemo alias veritatis id ut sed perspiciatis, sit odit sapiente exercitationem
+                        neque dolorem quisquam deleniti optio natus ad repudiandae officia? Non omnis quis illo dolor ea
+                        modi repellendus. Consequatur vero consequuntur et dignissimos, odit sed! Rem facere, officiis
+                        atque facilis eveniet consectetur necessitatibus magnam odit quod laborum magni corporis quo
+                        quibusdam et id repellat quisquam cupiditate sint eum impedit eaque doloribus quidem. Expedita
+                        hic voluptatibus accusamus eos et qui eius. Repellat fugit voluptate sit esse corporis cum illum
+                        provident saepe quod officiis officia, autem ullam ducimus! Asperiores tempore molestias
+                        necessitatibus ea, voluptate ullam fugit libero nulla nam deleniti mollitia vitae et fuga
+                        reprehenderit? At, cumque? Laudantium dicta nobis distinctio totam inventore atque voluptatibus
+                        neque hic veniam voluptate? Rerum eveniet inventore perspiciatis quod rem porro cupiditate unde
+                        aliquid? Atque necessitatibus, incidunt consequuntur ipsa aspernatur obcaecati at suscipit
+                        repellendus nemo ipsum deleniti error beatae iusto voluptates, esse saepe fugiat unde amet!
+                        Asperiores amet nulla sunt accusamus dolorum illo?
+                    </p>
+                </div>
+                <div id="users">
+                    <h1>Users</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla nesciunt consequatur esse tenetur
+                        culpa asperiores doloremque expedita odit veniam aliquid nostrum soluta saepe magni velit
+                        dolorum illo doloribus, rerum id. Voluptatum facere necessitatibus dolor aspernatur maiores est
+                        quaerat ad, ipsa ea distinctio neque expedita ab id iste, consequuntur fugit nostrum officia
+                        dolore molestias itaque suscipit aliquam iure saepe illo! Pariatur itaque dolorem maxime ipsa.
+                        Modi aspernatur vero, possimus necessitatibus culpa cumque reiciendis doloremque suscipit
+                        repudiandae, aperiam et officiis corrupti cum distinctio iure quia est. Sunt velit explicabo
+                        error aliquid expedita voluptatum quos itaque nemo ex veniam quibusdam nostrum ab odio maxime
+                        nisi a cumque aspernatur optio eum, earum culpa. Repudiandae officiis at eum deserunt enim
+                        perspiciatis! Sequi esse eos laborum cumque similique iure earum maxime itaque omnis assumenda
+                        pariatur laudantium iste aperiam vel eaque, perferendis doloremque adipisci, tempora reiciendis?
+                        Deleniti molestiae deserunt mollitia error provident nostrum perferendis et iure id, nobis nisi
+                        ducimus dolorem placeat odit exercitationem quae sint minima quibusdam eum similique voluptate.
+                        Officiis blanditiis iusto cumque eum omnis impedit quod ratione cupiditate recusandae quisquam
+                        corporis sapiente, quae, ullam ducimus repellat? Eveniet inventore officia saepe quis dolore ea
+                        consequuntur, aliquid, rem ab similique repellendus. Dolorum cumque a provident fuga.
+                    </p>
+                </div>
+            </article>
+        </main>
+
+        <!-- Footer section -->
+        <footer>
+            <form action="./handlecontact.php">
+                <div>
+                    <label for="">Name</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label for="">Email</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label for="">Message</label>
+                    <textarea name="" id="" cols="30" rows="10"></textarea>
+                </div>
+
+                <div>
+                    <input type="submit" value="Send">
+                </div>
+            </form>
+        </footer>
+    </div>
+
+</body>
+
+</html>
